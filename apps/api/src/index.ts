677f9b9dbe7c5db0dea1b0b8cb294ae21777920f
AkshitLakhera <akshitlakhera14@gmail.com>
2025-12-18 20:54:55 +0530

    Package installed

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";  
 import { app } from "./app";
 
-const PORT = Number(process.env.PORT) || 3001;
+const PORT = Number(process.env.PORT) || 3002;
 
 console.log("Starting server...");
 console.log("DB URL at Prisma init:", process.env.DATABASE_URL);
